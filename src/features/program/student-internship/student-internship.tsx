697d3ce816699bdfbc0f75c68bf42bc3bc0d88e1
onkids <ongkywijaya61@gmail.com>
2024-05-31 17:56:41 +0700

    change language english to indonesia

--- src/features/program/student-internship/student-internship.tsx
+++ src/features/program/student-internship/student-internship.tsx
@@ -1,6 +1,6 @@
 import { studentInternshipDataStatic } from '@/app/api/student-internship'
 import { AskedQuestion, Banner, FeaturedPrograms } from '@/features/shared'
-import { Flexbox, Markdown, Section, SectionTitle, Typo } from '@/nui'
+import { Markdown, Section, SectionTitle } from '@/nui'
 
 export default function StudentInternship() {
   const { info, title, contents } = studentInternshipDataStatic
@@ -11,14 +11,7 @@ export default function StudentInternship() {
         <SectionTitle sizeTitle="6xl" info={info} title={title} />
       </Section>
       <Section maxWidth="sm">
-        <Flexbox align="normal" flow="col" gap="2xl">
-          <Flexbox flow="col" gap="none" align="normal">
-            <Typo size="2xl" fontWeight="bold" color="gray-900">
-              {contents.title}
-            </Typo>
-            <Markdown size="lg">{contents.body}</Markdown>
-          </Flexbox>
-        </Flexbox>
+        <Markdown size="lg">{contents.body}</Markdown>
       </Section>
       <FeaturedPrograms variant="gray" />
       <AskedQuestion />
