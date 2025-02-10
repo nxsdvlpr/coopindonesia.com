6c2551daecaa965fbd1ade3da10156c4df024424
nxs-clarissa <nxs.clarissa@gmail.com>
2025-02-10 17:29:39 +0700

    add navbar-lang also change design ID | JA

--- src/nui/header/components/lang.tsx
+++ src/nui/header/components/lang.tsx
@@ -1,14 +1,15 @@
 'use client'
 
 import { useLocale } from 'next-intl'
+import Image from 'next/image'
 import { usePathname, useRouter } from 'next/navigation'
 import { tv } from 'tailwind-variants'
 
 export const menuLangButtonStyle = tv({
-  base: 'rounded px-1 py-0.5',
+  base: 'rounded px-1 py-0.5 font-semibold',
   variants: {
     variant: {
-      gray: 'bg-gray-100',
+      gray: 'text-gray-400',
       none: '',
     },
   },
@@ -20,24 +21,39 @@ export default function MenuLang() {
   const locale = useLocale()
 
   return (
-    <div className="flex items-center gap-x-2">
+    <div className="ml-4 flex items-center gap-x-2">
+      <Image
+        width={16}
+        height={16}
+        sizes="100vw"
+        src="/navbar-lang/emojione_flag-for-indonesia.svg"
+        alt="flag-indo"
+      />
       {
         <button
           className={menuLangButtonStyle({
-            variant: locale === 'in' ? 'gray' : 'none',
+            variant: locale === 'ja' ? 'gray' : 'none',
           })}
           onClick={() => router.push(`/in${pathname}`)}
         >
-          IND
+          ID
         </button>
       }
+      <p className="text-gray-400">|</p>
+      <Image
+        width={16}
+        height={16}
+        sizes="100vw"
+        src="/navbar-lang/emojione_flag-for-japan.svg"
+        alt="flag-japan"
+      />
       <button
         className={menuLangButtonStyle({
-          variant: locale === 'ja' ? 'gray' : 'none',
+          variant: locale === 'in' ? 'gray' : 'none',
         })}
         onClick={() => router.push(`/ja${pathname}`)}
       >
-        JPY
+        JA
       </button>
     </div>
   )
